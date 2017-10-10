b8479c854b11f207456aec3544bf6ecd8018c902
kivaschenko <civaschenko@yahoo.com>
2017-10-10 20:18:08 +0300

    Changes to EDA

--- titanic.py
+++ titanic.py
@@ -7,9 +7,8 @@
 # import libraries
 import numpy as np 
 import pandas as pd 
-from sklearn.cluster import KMeans
 import re
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score
@@ -26,13 +25,36 @@ submission_file = "{0}/gender_submission.csv".format(data_dir)
 df = pd.concat([pd.read_csv(train_file), pd.read_csv(test_file)])
 print(df.info())
 
+
+
 #   To fill missed values of main features:
 df["Embarked"].fillna("S", inplace=True)
 df["Age"].fillna(df["Age"].median(), inplace=True)
 df["Fare"].fillna(df["Fare"].median(), inplace=True)
+#  Assign cabin availibility
+df['has_cabin'] = df.loc[:,"Cabin"].apply(lambda x: 0 if type(x) == float else 1)
+#  Get length of names: name_length
+df['name_length'] = df['Name'].apply(len)
 
 #   To count the family size: 
-df['family_size'] = df['SibSp'] + df['Parch'] + 1
+df['family_size'] = df.loc[:,'SibSp'] + df.loc[:,'Parch'] + 1
+df['is_alone'] = 0
+df.loc[df['family_size'] == 1, 'is_alone'] = 1
+
+df['Sex'] = df['Sex'].map({'female': 0, 'male': 1}).astype(int)
+df['Embarked'] = df['Embarked'].map({'S': 0, 'C': 1, 'Q': 2}).astype(int)
+
+df.loc[ df['Fare'] <= 7.91, 'Fare']                               = 0
+df.loc[(df['Fare'] > 7.91) & (df['Fare'] <= 14.454), 'Fare'] = 1
+df.loc[(df['Fare'] > 14.454) & (df['Fare'] <= 31), 'Fare']   = 2
+df.loc[ df['Fare'] > 31, 'Fare']                                  = 3
+df['Fare'] = df['Fare'].astype(int)
+    
+df.loc[df['Age'] <= 16, 'Age'] = 0
+df.loc[(df['Age'] > 16) & (df['Age'] <= 32), 'Age'] = 1
+df.loc[(df['Age'] > 32) & (df['Age'] <= 48), 'Age'] = 2
+df.loc[(df['Age'] > 48) & (df['Age'] <= 64), 'Age'] = 3
+df.loc[df['Age'] > 64, 'Age'] = 4
 
 # Define function to extract titles from passenger names
 def get_title(name_pass):
@@ -43,7 +65,6 @@ def get_title(name_pass):
     return ""
 
 df['title'] = df['Name'].apply(get_title)
-
 
 # Group all non-common titles into one single grouping "Rare"
 df['title'] = df['title'].replace(['Lady', 
@@ -62,73 +83,68 @@ df['title'] = df['title'].replace('Mlle', 'Miss')
 df['title'] = df['title'].replace('Ms', 'Miss')
 df['title'] = df['title'].replace('Mme', 'Mrs')
 
-for key, value in zip(df.title.unique(), list(range(len(df.title.unique())))):
-    df.loc[df["title"] == key, "title"] = value
-
-#  Get length of names: name_length
-df['name_length'] = df['Name'].apply(len)
-
-
-# Get some clusters of the age and fare of passengers: age_fare_clusters
-age_fare_clusters = np.array(df.loc[:,['Age', 'Fare']])
-kmeans = KMeans()
-kmeans.fit(age_fare_clusters)
-df['age_fare_clusters'] = kmeans.labels_
+for name_column in ['Sex', 'Embarked', 'title']:
+    for key, value in zip(df[name_column].unique(), 
+        list(range(len(df[name_column].unique())))):
+        df.loc[df[name_column] == key, name_column] = value
+    df[name_column] = df[name_column].astype(float)
 
 
-# The binary features add:
-def get_dummie_columns(df, name_column):
-    """Convert to binar number of value categories current column"""
-    
-    df_dummie = pd.get_dummies(df[name_column][:], prefix=name_column)
-    df_dummie = pd.concat([df[:],df_dummie.iloc[:,:-1]], axis=1)
-    
-    return df_dummie
+df.drop(['Age',
+        'Fare', 
+        'Cabin', 
+        'Ticket',
+        'Name',
+        'name_length',
+        'SibSp',
+        'PassengerId'], axis=1, inplace=True)
 
-name_column = ['Sex','Embarked']
-
-#   The bypassing the needing column name list
-for col in name_column:
-    x = df.loc[:,:]
-    df = get_dummie_columns(x, col)
-    df.drop(col, axis=1, inplace=True)
-
-
-df.drop(['Cabin', 'Ticket', 'Name'], axis=1, inplace=True)
+print('df past dropping: {}'.format(df.info()))
 
 train = df[(df.Survived == 1) | (df.Survived == 0)]
 survived = train['Survived'][:].values
 print("The target set -- survived.shape:{}".format(survived.shape))
 
-train = train.drop(['PassengerId', 'Survived'], axis=1)
+train = train.drop('Survived', axis=1)
 print("The new train set:{}".format(train.head()))
 
 test = df[(df.Survived != 1) & (df.Survived != 0)]
-test.drop(['PassengerId', 'Survived'], axis=1, inplace=True)
+test.drop('Survived', axis=1, inplace=True)
 
 X = train.values
-y = survived
+y = survived.ravel()
 X_pred = test.values
 
-scaler = StandardScaler()
+scaler = MinMaxScaler()
 X_scaled = scaler.fit_transform(X)
 X_pred_scaled = scaler.transform(X_pred)
 
 print("Shape X_scaled: {0}. Shape y: {1}. Shape X_pred_scaled : {2}"\
       .format(X_scaled.shape, y.shape, X_pred_scaled.shape))
+print("X_scaled[:10]: {0}.\n\n\n y[:10]:{1}.\n\n\n X_pred_scaled[:10]:{2}" \
+    .format(X_scaled[:10], y[:10], X_pred_scaled[:10]))
 
-param_grid = {'n_estimators': [n for n in range(10, 110, 10)],
-              'max_features': [f for f in range(2, 12)]}
-print("Parameter grid:\n{}".format(param_grid))
-grid_search = GridSearchCV(RandomForestClassifier(random_state=42, n_jobs=-1), param_grid, cv=5)
+# param_grid = {'n_estimators': [n for n in range(20, 200, 20)],
+#               'max_depth'   : [d for d in range(4,10)],
+#               'max_features': [f for f in range(2, 7)]}
+# print("Parameter grid:\n{}".format(param_grid))
+# grid_search = GridSearchCV(RandomForestClassifier(random_state=42, 
+#                                                 n_jobs=-1), param_grid, cv=5)
 X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, random_state=21)
-grid_search.fit(X_train, y_train)
-print("Test set score: {:.5f}".format(grid_search.score(X_test, y_test)))
-print("Best parameters: {}".format(grid_search.best_params_))
-print("Best cross-validation score: {:.5f}".format(grid_search.best_score_))
-print("Best estimator:\n{}".format(grid_search.best_estimator_))
+# grid_search.fit(X_train, y_train)
+# print("Test set score: {:.5f}".format(grid_search.score(X_test, y_test)))
+# print("Best parameters: {}".format(grid_search.best_params_))
+# print("Best cross-validation score: {:.5f}".format(grid_search.best_score_))
+# print("Best estimator:\n{}".format(grid_search.best_estimator_))
 
-forest = grid_search.best_estimator_
+# forest = grid_search.best_estimator_
+
+forest = RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini',
+            max_depth=7, max_features=4, max_leaf_nodes=None,
+            min_impurity_decrease=0.0, min_impurity_split=None,
+            min_samples_leaf=1, min_samples_split=2,
+            min_weight_fraction_leaf=0.0, n_estimators=100, n_jobs=-1,
+            oob_score=False, random_state=42, verbose=0, warm_start=False)
 forest.fit(X_train, y_train)
 print("Accuracy on training set: {:.5f}".format(forest.score(X_train, y_train)))
 print("Accuracy on test set: {:.5f}".format(forest.score(X_test, y_test)))
@@ -142,4 +158,22 @@ print("Average cross-validation score: {:.5f}".format(scores.mean()))
 ################ To write the results in file ####################
 submission = pd.read_csv(submission_file)
 submission.iloc[:, 1] = forest.predict(X_pred_scaled)
-submission.to_csv('random_forest_clf_titanic_subm.csv', index=None)
+submission.to_csv('random_forest_clf_titanic_subm.csv', index=False)
+
+
+# Parameter grid:
+# {'n_estimators': [20, 40, 60, 80, 100, 120, 140, 160, 180], 'max_depth': [4, 5, 6, 7, 8, 9], 'max_features': [2, 3, 4, 5, 6]}
+# Test set score: 0.81166
+# Best parameters: {'n_estimators': 100, 'max_depth': 7, 'max_features': 4}
+# Best cross-validation score: 0.85479
+# Best estimator:
+# RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini',
+#             max_depth=7, max_features=4, max_leaf_nodes=None,
+#             min_impurity_decrease=0.0, min_impurity_split=None,
+#             min_samples_leaf=1, min_samples_split=2,
+#             min_weight_fraction_leaf=0.0, n_estimators=100, n_jobs=-1,
+#             oob_score=False, random_state=42, verbose=0, warm_start=False)
+# Accuracy on training set: 0.91617
+# Accuracy on test set: 0.81166
+# Cross-validation scores: [ 0.83240223  0.8258427   0.82022472  0.78651685  0.85393258]
+# Average cross-validation score: 0.82378
