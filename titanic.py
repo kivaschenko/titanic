087fe14daae451d04823492db4bf39efdcd8b6ce
kivaschenko <civaschenko@yahoo.com>
2017-10-10 07:56:51 +0300

    titanic script drop columns features

--- titanic.py
+++ titanic.py
@@ -61,6 +61,9 @@ df['title'] = df['title'].replace('Mlle', 'Miss')
 df['title'] = df['title'].replace('Ms', 'Miss')
 df['title'] = df['title'].replace('Mme', 'Mrs')
 
+for key, value in zip(df.title.unique(), list(range(len(df.title.unique())))):
+    df.loc[df["title"] == key, "title"] = value
+
 #  Get length of names: name_length
 df['name_length'] = df['Name'].apply(len)
 
@@ -77,17 +80,11 @@ def get_dummie_columns(df, name_column):
     """Convert to binar number of value categories current column"""
     
     df_dummie = pd.get_dummies(df[name_column][:], prefix=name_column)
-    df_dummie = pd.concat([df[:],df_dummie[:]], axis=1)
+    df_dummie = pd.concat([df[:],df_dummie.iloc[:,:-1]], axis=1)
     
     return df_dummie
 
-name_column = ['Sex',
-                'Pclass', 
-                'Embarked', 
-                'family_size', 
-                'age_fare_clusters', 
-                'title', 
-                'name_length']
+name_column = ['Sex','Embarked']
 
 #   The bypassing the needing column name list
 for col in name_column:
@@ -95,7 +92,6 @@ for col in name_column:
     df = get_dummie_columns(x, col)
     df.drop(col, axis=1, inplace=True)
 
-#print(df.info())
 
 df.drop(['Cabin', 'Ticket', 'Name'], axis=1, inplace=True)
 
@@ -104,7 +100,7 @@ survived = train['Survived'][:].values
 print("The target set -- survived.shape:{}".format(survived.shape))
 
 train = train.drop(['PassengerId', 'Survived'], axis=1)
-print("The shape of new train set:{}".format(train.shape))
+print("The new train set:{}".format(train.head()))
 
 test = df[(df.Survived != 1) & (df.Survived != 0)]
 test.drop(['PassengerId', 'Survived'], axis=1, inplace=True)
@@ -115,11 +111,11 @@ X_pred = test.values
 print("Shape X: {0}. Shape y: {1}. Shape X_pred : {2}"\
       .format(X.shape, y.shape, X_pred.shape))
 
-param_grid = {'n_estimators': [n for n in range(10, 100, 20)],
-              'max_features': [f for f in range(2, 12, 2)]}
+param_grid = {'n_estimators': [n for n in range(10, 110, 10)],
+              'max_features': [f for f in range(2, 12)]}
 print("Parameter grid:\n{}".format(param_grid))
-grid_search = GridSearchCV(RandomForestClassifier(random_state=21), param_grid, cv=5)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, random_state=42)
+grid_search = GridSearchCV(RandomForestClassifier(random_state=42, n_jobs=-1), param_grid, cv=5)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=21)
 grid_search.fit(X_train, y_train)
 print("Test set score: {:.5f}".format(grid_search.score(X_test, y_test)))
 print("Best parameters: {}".format(grid_search.best_params_))
@@ -141,3 +137,21 @@ print("Average cross-validation score: {:.5f}".format(scores.mean()))
 submission = pd.read_csv(submission_file)
 submission.iloc[:, 1] = forest.predict(X_pred)
 submission.to_csv('random_forest_clf_titanic_subm.csv', index=None)
+
+# Shape X: (891, 12). Shape y: (891,). Shape X_pred : (418, 12)
+# Parameter grid:
+# {'max_features': [2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 'n_estimators': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]}
+# Test set score: 0.83857
+# Best parameters: {'max_features': 5, 'n_estimators': 30}
+# Best cross-validation score: 0.84132
+# Best estimator:
+# RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini',
+#             max_depth=None, max_features=5, max_leaf_nodes=None,
+#             min_impurity_decrease=0.0, min_impurity_split=None,
+#             min_samples_leaf=1, min_samples_split=2,
+#             min_weight_fraction_leaf=0.0, n_estimators=30, n_jobs=-1,
+#             oob_score=False, random_state=42, verbose=0, warm_start=False)
+# Accuracy on training set: 0.98503
+# Accuracy on test set: 0.83857
+# Cross-validation scores: [ 0.7877095   0.80898876  0.81460674  0.79213483  0.84831461]
+# Average cross-validation score: 0.81035
